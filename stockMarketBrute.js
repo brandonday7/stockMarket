fea68ab06463b18ece4484bcbc757e4ad5797c1e
Brandon Day <brandontday7@gmail.com>
2017-09-08 18:49:49 +0000

    calculates largest possible stock profit using brute force method

---
+++ stockMarketBrute.js
@@ -0,0 +1,23 @@
+function maxProfit(stockPricesArray) {
+  let max = -1;
+  for (index in stockPricesArray)
+  {
+    if (index == stockPricesArray.length -1)
+    {
+      break;
+    }
+
+    for (let i = Number(index) + 1; i < stockPricesArray.length; i++)
+    {
+      let profit = stockPricesArray[i] - stockPricesArray[index];
+      if (profit > max)
+      {
+        max = profit;
+      }
+    }
+
+  }
+  return max;
+}
+
+console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
